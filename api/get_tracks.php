0bf8a789fac10c160cb47df49c86119f38c03f45
googleit2 <jack.hawinkels@gmail.com>
2021-07-03 18:01:50 +1200

    Added get_tracks.php API file.

---
+++ api/get_tracks.php
@@ -0,0 +1,9 @@
+<?php
+// This PHP file is requested whenever the website needs to search tracks, and/or load information about them
+
+// The following GET parameters can be applied;
+// tracK_id - The ID of the track
+// playlist_id - The ID of the playlist.
+// name - The name of the track
+
+// Results will be returned in array form, as multiple tracks may fit the filter.
