f690fd2b9fafd0b4f21b6f082ec200979b2321e6
googleit2 <jack.hawinkels@gmail.com>
2021-08-15 13:49:47 +1200

    Added calculation to get total duration

--- iframe/refined_search.php
+++ iframe/refined_search.php
@@ -8,6 +8,7 @@
         let playlist_type_el
 
         const updateResults = async () => {
+            let duration = 0
             if (playlist_type_el.value === "Artist") {
                 // Remove all currently showing items
                 for (let child of document.getElementsByClassName("sideways_slider_content")[0].children) {
@@ -20,6 +21,7 @@
                 let html = ""
                 for (let item of json) {
                     // Turn each item into an element
+                    duration += parseInt(item["duration"])
                     html += '<div class="sideways_slider_item"><div class="sideways_slider_icon play_animate" onclick="api.play_playlist()"> <img class="play_track_button" src="../assets/icons/play.svg"/> </div> <div class="playlist_title" onclick="alert(\'Viewing all tracks associated with an artist is currently unsupported. Try switching to albumns or genres\')">' + item["artist_name"] + '<br> <i> ' + item["count"] + ' tracks</i> </div> </div>'
                 }
                 document.getElementsByClassName("sideways_slider_content")[0].innerHTML = html
@@ -40,10 +42,31 @@
                 let html = ""
                 for (let item of json) {
                     // Turn each item into an element
+                    duration += parseInt(item["duration"])
                     html += '<div class="sideways_slider_item"><div class="sideways_slider_icon play_animate" onclick="api.play_playlist()"> <img class="play_track_button" src="../assets/icons/play.svg"/> </div> <div class="playlist_title" onclick="api.send_iframe_to_page(\'iframe/playlist.php?playlist_id=' + item["playlist_id"] + '\')">' + item["title"] + '<br> <i> ' + item["count"] + ' tracks</i> </div> </div>'
                 }
                 document.getElementsByClassName("sideways_slider_content")[0].innerHTML = html
             }
+            console.log(duration)
+
+            // Transform the duration
+            let hours = 0
+            let minutes = 0
+            let seconds = 0
+
+            while(duration >= 3600) {
+                duration -= 3600
+                hours += 1
+            }
+
+            while (duration >= 60) {
+                duration -= 60
+                minutes += 1
+            }
+
+            seconds = duration
+
+            document.getElementById("duration").innerHTML = "Total Duration: " + hours + ":" + minutes + ":" + seconds + " (Duration if all shown results were played back-to-back. Some tracks may be repeated.)"
         }
 
         const onload = () => {
@@ -68,6 +91,7 @@
                 <option>Genre</option>
             </select>
         </form>
+        <div id="duration"></div>
         <div class="sideways_slider">
             <div style="overflow: unset; height: unset; display: unset;" class="sideways_slider_content">
 
